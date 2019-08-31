401d746fc2d28bf4132194f55518b198e6b8fdd0
golden-orange <slaing@hotmail.com>
2019-08-31 17:16:54 -0400

    adding final bits of chapter 6

---
+++ lenslocked.com/ch_06/06_6/views/view.go
@@ -0,0 +1,40 @@
+package views
+
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+// use glob to call all files within a directory, rather than by individual name
+// reduces need to manage files individually
+// utilizes many-to-few management, or lever/leverage
+func NewView(layout string, files ...string) *View {
+	files = append(files, layoutFiles()...)
+
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
+
+type View struct {
+	Template *template.Template
+	Layout   string
+}
