64092bf05dc271cb14265de5486e40c91478fee4
golden-orange <slaing@hotmail.com>
2019-09-03 15:36:38 -0400

    adding CRUD and HTTP methods with gorilla MUX

---
+++ lenslocked.com/ch_07/07_4/controllers/users.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"lenslocked.com/ch_07/07_3/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+// New is used to render the form where a user can create a new user account
+// handler or "action"
+//
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// Create is used to process the signup form when a user submits/tries
+// to create a new user account.
+//
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response.")
+}
+
+type Users struct {
+	NewView *views.View
+}
