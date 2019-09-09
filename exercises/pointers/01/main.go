e451d012cfac6ab77439316a0089809b666a7dac
golden-orange <slaing@hotmail.com>
2019-09-09 09:03:25 -0400

    yet another quick update to pointer practice

--- exercises/pointers/01/main.go
+++ exercises/pointers/01/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	v := 43
-	w := v
-	vw := &v
-	ww := &w
-	wv := *&v
-	*vw = 57
+	v := 43   // v contains int 43
+	w := v    // w contains int 43
+	vw := &v  // vw contains address of v
+	ww := &w  // ww contains address of w
+	wv := *&v // wv contains int 43
+	*vw = 57  // changes v and wv to int 57, but w remains containing int 43
 
 	fmt.Println("'v'\t", v)
 	fmt.Println("w := v", w)
@@ -18,12 +18,7 @@ func main() {
 	fmt.Println("'*vw' points to the value stored 'vw' which is an address containing '43':", *vw)
 	fmt.Println("'*ww' points to the value stored in the address of w which is assigned the value of 'v' which is '43':", *ww)
 	fmt.Println("\n\n,'wv := *&v': assigns to 'wv' a pointer to the value in the address of v thus returning '43':", wv)
-	fmt.Println("\n\n, '*vw = 57' assigns the value '57' to the address &vw therefore changing the value in the original location 'v'. '*vw' is '57':", *vw, "and 'v' is also '57':", v)
-	av := &v
-	fmt.Println("'& points to the address of a variable. Therefore:'")
-	fmt.Println("'av := &v'", av)
-
-	pv := *av
-	fmt.Println("'*' points to the value in the address. Therefore:")
-	fmt.Println("'pv := *av'", pv)
+	fmt.Println("\n, '*vw = 57' assigns the value '57' to the address &vw therefore changing the value in the original location 'v'. '*vw' is '57':", *vw, "and 'v' is also '57':", v)
+	fmt.Println("\n, However, 'w' still contains the original value of 'v':", w)
+	fmt.Println("\n, And 'wv' which contains the value in the address of 'v' (*&v) is changed to '57':", *&v)
 }
