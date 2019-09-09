d1743b8d73a8b68a3df159ed3573bcfc121d8a9c
golden-orange <slaing@hotmail.com>
2019-09-09 08:46:02 -0400

    updating pointer practice file

--- exercises/pointers/01/main.go
+++ exercises/pointers/01/main.go
@@ -8,16 +8,17 @@ func main() {
 	vw := &v
 	ww := &w
 	wv := *&v
+	*vw = 57
 
-	fmt.Println("'v'", v)
+	fmt.Println("'v'\t", v)
 	fmt.Println("w := v", w)
 	fmt.Println("'vw := &v' assigns to 'vw' the address of 'v':", vw)
 	fmt.Println("'ww := &w' assigns to 'ww' the address of 'w':", ww)
 	fmt.Printf("'vw' points to the address of v, and is therefore of type %T:", vw)
 	fmt.Println("'*vw' points to the value stored 'vw' which is an address containing '43':", *vw)
 	fmt.Println("'*ww' points to the value stored in the address of w which is assigned the value of 'v' which is '43':", *ww)
-	fmt.Println("'wv := *&v': assigns to 'wv' a pointer to the value in the address of v thus returning '43':", wv)
-
+	fmt.Println("\n\n,'wv := *&v': assigns to 'wv' a pointer to the value in the address of v thus returning '43':", wv)
+	fmt.Println("\n\n, '*vw = 57' assigns the value '57' to the address &vw therefore changing the value in the original location 'v'. '*vw' is '57':", *vw, "and 'v' is also '57':", v)
 	av := &v
 	fmt.Println("'& points to the address of a variable. Therefore:'")
 	fmt.Println("'av := &v'", av)
