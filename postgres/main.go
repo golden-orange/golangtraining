8042751018d34b115c32aa410490d68145c34134
golden-orange <findme@scottlaing.ca>
2019-09-23 08:40:22 -0400

    adding postgres practice file

---
+++ postgres/main.go
@@ -0,0 +1,40 @@
+package main
+import (
+  "fmt"
+  "database/sql"
+
+  _ "github.com/lib/pq"
+)
+
+const (
+  host = "localhost"
+  port = 5432
+  user = "postgres"
+  dbname = "postgres"
+  password = "re300ter"
+)
+
+func main()  {
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+  "password=%s dbname=%s sslmode=disable",
+  host, port, user, password, dbname)
+
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  sqlStatement := `
+INSERT INTO users (age, email, first_name, last_name)
+VALUES ($1, $2, $3, $4)
+RETURNING id`
+  id := 0
+  err = db.QueryRow(sqlStatement, 53, "findme@scottlaing.ca", "Scott", "Laing").Scan(&id)
+
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Println("New record ID is:", id)
+}
